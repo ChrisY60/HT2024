c0aafd304b3e44390ce3d7249c7ae47569fa0dc9
Емилия Ушева <emiliyausheva730@gmail.com>
2024-03-15 11:01:08 +0200

    fixed dynamic registration, login and subject fetching

--- frontend/htx/src/components/Main.jsx
+++ frontend/htx/src/components/Main.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import {useAuth} from "./Auth";
+import axios from "axios";
 
 const Main = () => {
-    const subjects = [
+    const [subjects, setSubjects] = useState([]);
+    const { token } = useAuth();
+    /*const subjects = [
         {
             title: 'Mathematics',
             description: 'Explore the world of numbers and equations.',
@@ -27,7 +31,19 @@ const Main = () => {
             imageURL: 'https://d3nl1jd9up78ug.cloudfront.net/wages/how-to-become-a-mathematician-484-detail.jpg',
             link: '/subject'
         }
-    ];
+    ];*/
+
+    useEffect(() => {
+        const headers = {Authorization: `Bearer ${token}`};
+        axios.get(`http://localhost:8080/api/v1/subjects`, {headers})
+            .then((res) => {
+                const subjectData = res.data;
+                setSubjects(subjectData);
+            })
+            .catch((err) => {
+                console.error('Error fetching subjects:', err);
+            });
+    },[]);
 
     return (
         <div className="container mt-5">
@@ -35,11 +51,10 @@ const Main = () => {
             <div className="row">
                 {subjects.map((subject, index) => (
                     <div key={index} className="col-lg-6 mb-4">
-                        <Link to={subject.link} className="card d-flex flex-column justify-content-center align-items-center" style={{ height: '20vh', width: '40vw', overflow: 'hidden', paddingRight: 0, paddingLeft: 0, textDecoration: 'none' }}>
-                            <img src={subject.imageURL} className="card-img-top" alt={subject.title} style={{ height: '50%', objectFit: 'cover' }} />
+                        <Link to={`/subject/${subject.id}`} className="card d-flex flex-column justify-content-center align-items-center" style={{ height: '20vh', width: '40vw', overflow: 'hidden', paddingRight: 0, paddingLeft: 0, textDecoration: 'none' }}>
+                            <img src={subject.imageURL} className="card-img-top" alt={subject.name} style={{ height: '50%', objectFit: 'cover' }} />
                             <div className="card-body text-center">
-                                <h5 className="card-title">{subject.title}</h5>
-                                <p className="card-text">{subject.description}</p>
+                                <h5 className="card-title">{subject.name}</h5>
                             </div>
                         </Link>
                     </div>
--- frontend/htx/src/components/Register.jsx
+++ frontend/htx/src/components/Register.jsx
@@ -11,20 +11,42 @@ const Register = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [school, setSchool] = useState('');
+    const [accessCode, setAccessCode] = useState('');
+    const [role, setRole] = useState('');
+    const [studentClass, setStudentClass] = useState(0);
+    const [studentClassLetter, setStudentClassLetter] = useState('');
+    const [classNumber, setClassNumber] = useState(0);
     const { login } = useAuth();
 
     const handleRegister = async () => {
         try {
+            var response;
             const [firstName, middleName, lastName] = name.split(' ');
+            const studentClassInfo = studentClass + studentClassLetter;
 
-            const response = await axios.post(`http://localhost:8080/api/v1/auth/register`, {
-                firstName: firstName,
-                middleName: middleName,
-                lastName: lastName,
-                email: email,
-                password: password,
-                school: school
-            });
+            if(role === 'STUDENT') {
+                response = await axios.post(`http://localhost:8080/api/v1/auth/register-student`, {
+                    firstName: firstName,
+                    middleName: middleName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                    school: school,
+                    accessCode: accessCode,
+                    classNumber: classNumber,
+                    studentClass: studentClassInfo,
+                });
+            } else {
+                response = await axios.post(`http://localhost:8080/api/v1/auth/register-teacher`, {
+                    firstName: firstName,
+                    middleName: middleName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                    school: school,
+                    accessCode: accessCode,
+                    role: role,
+            });}
 
             const token = response.data.token;
 
@@ -36,8 +58,9 @@ const Register = () => {
         }
     };
 
+    //TODO: field for accesscode, field for role, if role teacher nothing happens, if student - two dropdowns for class, one for class number
     return (
-        <div className="d-flex align-items-center justify-content-center vh-100">
+        <div className="d-flex align-items-center justify-content-center">
             <div className="card bg-white p-4 rounded-lg shadow-sm">
                 <h1 className="text-3xl font-weight-bold mb-4">Register</h1>
                 <form>
@@ -53,11 +76,11 @@ const Register = () => {
                     <div className="mb-2">
                         <label className="form-label">School:</label>
                         <select
-                            className="form-control" // Applying the same class as other inputs
+                            className="form-control"
                             value={school}
                             onChange={(e) => setSchool(e.target.value)}
                         >
-                            <option selected>Choose...</option>
+                            <option value="">Choose...</option>
                             <option value="TUES">TUES</option>
                             <option value="2">Two</option>
                             <option value="3">Three</option>
@@ -90,6 +113,77 @@ const Register = () => {
                             className="form-control"
                         />
                     </div>
+                    <div className="mb-2">
+                        <label className="form-label">Role:</label>
+                        <select
+                            className="form-control"
+                            value={role}
+                            onChange={(e) => setRole(e.target.value)}
+                        >
+                            <option value="">Choose...</option>
+                            <option value="TEACHER">Teacher</option>
+                            <option value="STUDENT">Student</option>
+                        </select>
+                    </div>
+                    <div className="mb-2">
+                        <label className="form-label">Access Code:</label>
+                        <input
+                            type="text"
+                            value={accessCode}
+                            onChange={(e) => setAccessCode(e.target.value)}
+                            className="form-control"
+                        />
+                    </div>
+                    {role === 'STUDENT' && (
+                        <>
+                            <div className="mb-2">
+                                <label className="form-label">Class:</label>
+                                <select
+                                    className="form-control"
+                                    value={studentClass}
+                                    onChange={(e) => setStudentClass(e.target.value)}
+                                >
+                                    <option value="">Choose...</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                </select>
+                            </div>
+                            <div className="mb-2">
+                                <label className="form-label">Class Letter:</label>
+                                <select
+                                    className="form-control"
+                                    value={studentClassLetter}
+                                    onChange={(e) => setStudentClassLetter(e.target.value)}
+                                >
+                                    <option value="">Choose...</option>
+                                    <option value="a">a</option>
+                                    <option value="B">B</option>
+                                    <option value="V">V</option>
+                                    <option value="G">G</option>
+                                </select>
+                            </div>
+                            <div className="mb-2">
+                                <label className="form-label">Class Number:</label>
+                                <input
+                                    type="number"
+                                    placeholder="Class Number"
+                                    className="form-control"
+                                    value={classNumber}
+                                    onChange={(e) => setClassNumber(e.target.value)}
+                                />
+                            </div>
+                        </>
+                    )}
                     <button
                         type="button"
                         onClick={handleRegister}
